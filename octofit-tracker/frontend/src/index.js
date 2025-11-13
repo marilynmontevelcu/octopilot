69a7b572c6eb1432a30dc093727e32b18410ac38
Marilyn <marilynmontevelcu@users.noreply.github.com>
2025-11-13 03:48:58 +0000

    Add React components for Activities, Leaderboard, Teams, Users, Workouts; update App.js and index.js for API integration and navigation

--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Set REACT_APP_CODESPACE_NAME from environment if available
+if (!process.env.REACT_APP_CODESPACE_NAME && window.location.hostname.includes('app.github.dev')) {
+  const codespace = window.location.hostname.split('-8000')[0];
+  process.env.REACT_APP_CODESPACE_NAME = codespace;
+  console.log('Detected Codespace:', codespace);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
